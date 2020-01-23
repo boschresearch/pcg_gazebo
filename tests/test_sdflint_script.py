6bbc5c2fb74a7290a3975840344e2dd95fecc406
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2020-01-23 17:50:53 +0100

    Add test for the sdflint script
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

---
+++ tests/test_sdflint_script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright (c) 2020 - The Procedural Generation for Gazebo authors
+# For information on the respective copyright owner see the NOTICE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+import pytest
+import os
+
+
+@pytest.mark.script_launch_mode('subprocess')
+def test_run_linter_on_test_files(script_runner):
+    test_folder = os.path.join(
+        os.path.abspath(__file__),
+        'sdf')
+    for filename in os.listdir(test_folder):
+        if '.sdf' not in filename:
+            continue
