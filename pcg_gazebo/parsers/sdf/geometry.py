f9138a1edd1fa0c16dba6c9597f962da035cf230
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2020-01-24 14:24:12 +0100

    Random choice of type of geometry
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- pcg_gazebo/parsers/sdf/geometry.py
+++ pcg_gazebo/parsers/sdf/geometry.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import random
 from ..types import XMLBase
 from .box import Box
 from .empty import Empty
@@ -128,6 +128,7 @@ class Geometry(XMLBase):
 
     def random(self):
         try:
+            self.reset(mode=random.choice(self._MODES))
             getattr(self, self._mode).random()
         except NotImplementedError:
             pass
