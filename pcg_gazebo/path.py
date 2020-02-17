2d501a077de32b28220369218dbd0d4599a045dd
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2020-02-17 13:19:12 +0100

    Add support for ament_index_python
    Support search for resource paths for ROS2 components
    
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- pcg_gazebo/path.py
+++ pcg_gazebo/path.py
@@ -14,9 +14,19 @@
 # limitations under the License.
 import os
 import re
-import rospkg
 from .log import PCG_ROOT_LOGGER
 from .utils import is_string
+try:
+    import rospkg
+    ROS1_AVAILABLE = True
+except ImportError:
+    ROS1_AVAILABLE = False
+
+try:
+    import ament_index_python
+    ROS2_AVAILABLE = True
+except ImportError:
+    ROS2_AVAILABLE = False
 
 
 class Path(object):
@@ -96,7 +106,7 @@ class Path(object):
         if self._ros_pkg is None:
             return None
         relative_path = self._absolute_uri.replace(
-            rospkg.RosPack().get_path(self._ros_pkg), '')
+            self._find_ros_package_resources_path(self._ros_pkg), '')
         prefix = 'package://{}'.format(self._ros_pkg)
         if relative_path[0] != '/':
             prefix += '/'
@@ -120,7 +130,7 @@ class Path(object):
         if self._ros_pkg is None:
             return None
         relative_path = self._absolute_uri.replace(
-            rospkg.RosPack().get_path(self._ros_pkg),
+            self._find_ros_package_resources_path(self._ros_pkg),
             '')
         prefix = '$(find {})'.format(self._ros_pkg)
         if relative_path[0] != '/':
@@ -128,16 +138,26 @@ class Path(object):
         return prefix + relative_path
 
     def _get_ros_package_name(self, filename):
-        finder = rospkg.RosPack()
-        if os.path.isfile(filename):
-            for pkg_name in finder.list():
-                if finder.get_path(pkg_name) in filename:
-                    return pkg_name
+        if ROS1_AVAILABLE:
+            finder = rospkg.RosPack()
+            if os.path.isfile(filename):
+                for pkg_name in finder.list():
+                    if finder.get_path(pkg_name) in filename:
+                        return pkg_name
+        else:
+            pkgs = ament_index_python.get_packages_with_prefixes()
+            if os.path.isfile(filename):
+                for pkg_name in pkgs.keys():
+                    if pkgs[pkg_name] in filename:
+                        return pkg_name
         return None
 
     def resolve_uri(self, uri):
         from .simulation import get_gazebo_model_path, \
             get_gazebo_model_ros_pkg
+        if uri is None:
+            PCG_ROOT_LOGGER.error('Provided URI is invalid')
+            return None
         if uri.startswith('${PCG}/') or \
                 uri.startswith('$PCG/') or \
                 uri.startswith('$(PCG)/'):
@@ -151,7 +171,7 @@ class Path(object):
             filename = os.path.join(PCG_ROOT_FOLDER, filename)
         elif os.path.isfile(os.path.abspath(uri)):
             return os.path.abspath(uri)
-        elif 'file://' in uri:
+        elif uri.startswith('file://'):
             filename = uri.replace('file://', '')
             if os.path.isfile(filename):
                 self._ros_pkg = self._get_ros_package_name(filename)
@@ -160,7 +180,7 @@ class Path(object):
                 msg = 'File {} does not exist'.format(filename)
                 PCG_ROOT_LOGGER.warning(msg)
                 return None
-        elif 'model://' in uri:
+        elif uri.startswith('model://'):
             self._gazebo_model = uri.replace('model://', '').split('/')[0]
             model_path = get_gazebo_model_path(self._gazebo_model)
             if model_path is None:
@@ -172,7 +192,7 @@ class Path(object):
                 'model://{}'.format(self._gazebo_model), model_path)
             self._ros_pkg = self._get_ros_package_name(filename)
             return filename
-        elif 'package://' in uri:
+        elif uri.startswith('package://'):
             result = re.findall(r'package://\w+/', uri)
             if len(result) != 1:
                 msg = 'Invalid package path for provided mesh uri {}'.format(
@@ -181,14 +201,14 @@ class Path(object):
                 raise ValueError(msg)
             self._ros_pkg = result[0].replace(
                 'package://', '').replace('/', '')
-            if self._ros_pkg not in rospkg.RosPack().list():
+            if not self._is_ros_package(self._ros_pkg):
                 msg = 'Package {} was not found, uri={}'.format(
                     self._ros_pkg, uri)
                 PCG_ROOT_LOGGER.warning(msg)
                 return None
-            pkg_path = rospkg.RosPack().get_path(self._ros_pkg)
+            pkg_path = self._find_ros_package_resources_path(self._ros_pkg)
             return uri.replace(result[0], pkg_path + '/')
-        elif '$(find' in uri:
+        elif uri.startswith('$(find'):
             uri_temp = uri.replace('$', '')
             result = re.findall(r'(find \w+)', uri_temp)
             if len(result) == 0:
@@ -199,17 +219,36 @@ class Path(object):
 
             self._ros_pkg = result[0].split()[1]
 
-            if self._ros_pkg not in rospkg.RosPack().list():
+            if not self._is_ros_package(self._ros_pkg):
                 msg = 'Package {} was not found, uri={}'.format(
                     self._ros_pkg, uri)
                 PCG_ROOT_LOGGER.warning(msg)
                 return None
-            pkg_path = rospkg.RosPack().get_path(self._ros_pkg)
+            pkg_path = self._find_ros_package_resources_path(self._ros_pkg)
             return uri_temp.replace(
                 '(find {})'.format(
                     self._ros_pkg), pkg_path)
         else:
             return None
+
+    def _find_ros_package_resources_path(self, pkg):
+        if not ROS1_AVAILABLE and not ROS2_AVAILABLE:
+            return None
+
+        ros_package_path = None
+        if ROS1_AVAILABLE:
+            try:
+                ros_package_path = rospkg.RosPack().get_path(pkg)
+            except rospkg.ResourceNotFound:
+                pass
+
+        if ROS2_AVAILABLE:
+            try:
+                ros_package_path = \
+                    ament_index_python.get_package_share_directory(pkg)
+            except ament_index_python.PackageNotFoundError:
+                pass
+        return ros_package_path
 
     def _resolve_gazebo_model(self):
         from .simulation import get_gazebo_model_names, \
@@ -223,8 +262,25 @@ class Path(object):
                 return
 
     def _resolve_ros_package(self):
-        for ros_pkg in rospkg.RosPack().list():
-            pkg_path = rospkg.RosPack().get_path(ros_pkg)
+        pkgs = list()
+        if ROS1_AVAILABLE:
+            pkgs = pkgs + list(rospkg.RosPack().list())
+        elif ROS2_AVAILABLE:
+            pkgs = pkgs + list(
+                ament_index_python.get_packages_with_prefixes().keys())
+
+        for ros_pkg in pkgs:
+            pkg_path = self._find_ros_package_resources_path(ros_pkg)
             if pkg_path in os.path.dirname(self.absolute_uri):
                 self._ros_pkg = ros_pkg
                 return
+
+    def _is_ros_package(self, pkg):
+        if ROS1_AVAILABLE:
+            if pkg in rospkg.RosPack().list():
+                return True
+        if ROS2_AVAILABLE:
+            if pkg in \
+                    ament_index_python.get_packages_with_prefixes().keys():
+                return True
+        return False
