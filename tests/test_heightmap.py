b32fd31e11b5f03385b970db6611e2464196391d
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2020-06-26 16:05:55 +0200

    Add import and export from SDF unit tests
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- tests/test_heightmap.py
+++ tests/test_heightmap.py
@@ -16,7 +16,10 @@
 import os
 import unittest
 from pcg_gazebo.parsers import parse_sdf
-from pcg_gazebo.simulation import add_custom_gazebo_resource_path
+from pcg_gazebo.simulation import add_custom_gazebo_resource_path, \
+    SimulationModel
+from pcg_gazebo.simulation.properties import Heightmap
+from pcg_gazebo.generators import HeightmapGenerator
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -32,6 +35,124 @@ class TestHeightmap(unittest.TestCase):
                 CUR_DIR, '..', 'examples', 'models',
                 'pcg_winding_valley_heightmap', 'model.sdf'))
         self.assertIsNotNone(sdf)
+        self.assertIsNotNone(sdf.models)
+        self.assertEqual(len(sdf.models[0].links[0].visuals), 1)
+        self.assertEqual(len(sdf.models[0].links[0].collisions), 1)
+
+        sdf_heightmap = sdf.models[0].links[0].visuals[0].geometry.heightmap
+        self.assertIsNotNone(sdf_heightmap)
+
+        uri_path_prefix = \
+            'model://pcg_winding_valley_heightmap/materials/textures'
+        self.assertIn(
+            uri_path_prefix,
+            sdf_heightmap.uri.value)
+        self.assertEqual(len(sdf_heightmap.textures), 3)
+        for texture in sdf_heightmap.textures:
+            self.assertIn(uri_path_prefix, texture.diffuse.value)
+            self.assertIn(uri_path_prefix, texture.normal.value)
+            self.assertEqual(texture.size.value, [10])
+        self.assertEqual(len(sdf_heightmap.blends), 2)
+        for blend in sdf_heightmap.blends:
+            self.assertGreater(blend.min_height.value, 0)
+            self.assertGreater(blend.fade_dist.value, 0)
+        self.assertEqual(sdf_heightmap.size.value, [1000, 1000, 25])
+        self.assertEqual(sdf_heightmap.pos.value, [0, 0, -4])
+
+        sdf_heightmap = \
+            sdf.models[0].links[0].collisions[0].geometry.heightmap
+        self.assertIsNotNone(sdf_heightmap)
+
+        self.assertIn(
+            uri_path_prefix,
+            sdf_heightmap.uri.value)
+        self.assertEqual(sdf_heightmap.size.value, [1000, 1000, 25])
+        self.assertEqual(sdf_heightmap.pos.value, [0, 0, -4])
+
+    def test_from_sdf(self):
+        add_custom_gazebo_resource_path(
+            os.path.join(CUR_DIR, '..', 'examples', 'models'))
+
+        # Parse the SDF file
+        sdf = parse_sdf(
+            os.path.join(
+                CUR_DIR, '..', 'examples', 'models',
+                'pcg_winding_valley_heightmap', 'model.sdf'))
+        self.assertIsNotNone(sdf)
+
+        # Convert SDF heightmap element to simulation heightmap element
+        sdf_heightmap = sdf.models[0].links[0].visuals[0].geometry.heightmap
+        heightmap = Heightmap.from_sdf(sdf_heightmap)
+        self.assertIsNotNone(heightmap)
+
+        output_sdf = heightmap.to_sdf()
+        self.assertIsNotNone(output_sdf)
+
+        sdf_heightmap = \
+            sdf.models[0].links[0].collisions[0].geometry.heightmap
+        heightmap = Heightmap.from_sdf(sdf_heightmap)
+        self.assertIsNotNone(heightmap)
+
+        output_sdf = heightmap.to_sdf()
+        self.assertIsNotNone(output_sdf)
+
+        self.assertEqual(sdf_heightmap.size.value, output_sdf.size.value)
+        self.assertEqual(sdf_heightmap.pos.value, output_sdf.pos.value)
+
+        # Convert model to SimulationModel element
+        model = SimulationModel.from_sdf(sdf.models[0])
+        self.assertIsNotNone(model)
+        output_sdf = model.to_sdf()
+        self.assertIsNotNone(output_sdf)
+
+        self.assertIsNotNone(output_sdf)
+        self.assertEqual(len(output_sdf.links[0].visuals), 1)
+        self.assertEqual(len(output_sdf.links[0].collisions), 1)
+
+        sdf_heightmap = output_sdf.links[0].visuals[0].geometry.heightmap
+        self.assertIsNotNone(sdf_heightmap)
+
+        uri_path_prefix = \
+            'model://pcg_winding_valley_heightmap/materials/textures'
+        self.assertIn(
+            uri_path_prefix,
+            sdf_heightmap.uri.value)
+        self.assertEqual(len(sdf_heightmap.textures), 3)
+        for texture in sdf_heightmap.textures:
+            self.assertIn(uri_path_prefix, texture.diffuse.value)
+            self.assertIn(uri_path_prefix, texture.normal.value)
+            self.assertEqual(texture.size.value, [10])
+        self.assertEqual(len(sdf_heightmap.blends), 2)
+        for blend in sdf_heightmap.blends:
+            self.assertGreater(blend.min_height.value, 0)
+            self.assertGreater(blend.fade_dist.value, 0)
+        self.assertEqual(sdf_heightmap.size.value, [1000, 1000, 25])
+        self.assertEqual(sdf_heightmap.pos.value, [0, 0, -4])
+
+        sdf_heightmap = \
+            output_sdf.links[0].collisions[0].geometry.heightmap
+        self.assertIsNotNone(sdf_heightmap)
+
+        self.assertIn(
+            uri_path_prefix,
+            sdf_heightmap.uri.value)
+        self.assertEqual(sdf_heightmap.size.value, [1000, 1000, 25])
+        self.assertEqual(sdf_heightmap.pos.value, [0, 0, -4])
+
+    def test_init_heightmap_generator_from_sdf(self):
+        add_custom_gazebo_resource_path(
+            os.path.join(CUR_DIR, '..', 'examples', 'models'))
+
+        # Parse the SDF file
+        sdf = parse_sdf(
+            os.path.join(
+                CUR_DIR, '..', 'examples', 'models',
+                'pcg_winding_valley_heightmap', 'model.sdf'))
+        self.assertIsNotNone(sdf)
+
+        sdf_heightmap = sdf.models[0].links[0].visuals[0].geometry.heightmap
+        hg = HeightmapGenerator.from_sdf(sdf_heightmap)
+        self.assertIsNotNone(hg)
 
 
 if __name__ == '__main__':
